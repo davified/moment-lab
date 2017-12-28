6573aad0cf4aee6d59453454ac97e4b39b795a57
davidtan <davidtan@thoughtworks.com>
2017-12-28 10:29:33 +0800

    Add assignment

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# JumpStart Assignment Template
+# Moment Lab
 
-[Insert Description]
+** It's time (pun intended) to learn! **
+
+This assignment requires you to complete a series of time manipulation tasks using `moment.js`, a library that most of you probably haven't encountered before.
 
 ### Getting Started
 - Fork this repo
 - `git clone <repo-url>` 
 - `cd` into directory
-- [insert any other setup instructions (e.g. `npm install`)]
+- Run `npm -v` to check if npm is installed
+- `npm install`
 
-[Insert assignment instructions]
+### Instructions
 
+Complete the tasks listed in `index.js`. Don't forget to refer to the [docs](https://momentjs.com/docs/). (hint: answers are all in the docs!)
---
+++ index.js
@@ -0,0 +1,63 @@
+const moment = require('moment');
+
+// 1. Warming up
+
+function timeNow() {
+    // return the current date and time in the moment.js default format (e.g. 2017-12-28T09:58:58+08:00)
+}
+
+function myBirthday() {
+    // return your birthday in the moment.js default format (e.g. 1985-12-28T00:00:00+08:00)
+}
+
+function todaysLunchtime() {
+    // return today's date and lunchtime 
+}
+
+// 2. Getting dates
+function thisYear() {
+    // to return the current year 
+} 
+
+function thisMonth() {
+    // to return the current month 
+} 
+function thisDay() {
+    // to return the current day of the year 
+} 
+
+// 3. Manipulating dates
+function FiveDaysFromToday() {
+    // return a date 5 days from today's date
+}
+
+function ThreeMonthsFromToday() {
+    // return a date 3 months from today's date
+}
+
+function FiveDaysAndThreeMonthsFromToday() {
+    // return a date 5 days and 3 months from today's date
+}
+
+function FiveDaysAgoFromToday() {
+    // return a date 5 days ago from today's date
+}
+
+
+// 4. formatting dates
+function currentTime() {
+    // return the current time in HH:mm format (e.g. 12:30)
+}
+
+function currentDateAndTimeInEnglish() {
+    // return the current time in HH:mm format (e.g. "Monday, January 31st 2018, 1:30:50 pm")
+}
+
+function dayAndHour() {
+    // return the current day and time (e.g. Monday, 1:30pm)
+}
+
+// 5. Working with Unix time
+function getCurrentTimeInUnixSecondsFormat() {
+    // return current date and time in unix format (e.g. 1514428000)
+}
